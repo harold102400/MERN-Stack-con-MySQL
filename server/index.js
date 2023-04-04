9e5b03e1bf44fd3cf2cf50c71376f6110a91d5c7
Harold Montes <haroldjosehd24@gmail.com>
2023-04-04 08:22:23 -0400

    project setup

---
+++ server/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import {PORT} from './config.js'
+import indexRoutes from './routes/index.routes.js'
+
+const app = express();
+
+app.listen(PORT)
+console.log(`Server is running on port ${PORT}`);
