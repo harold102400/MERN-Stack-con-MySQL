c0c258b4cd138dd37db9e4e4257d058f3f294cb2
Harold Montes <haroldjosehd24@gmail.com>
2023-04-04 22:54:02 -0400

    creating get tasks endpoint

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -1,27 +1,39 @@
-import { pool } from '../db.js';
+import { pool } from "../db.js";
 
-export const getTasks = (req, res) => {
-    res.send('getting tasks')
-}
+export const getTasks = async (req, res) => {
+  const [result] = await pool.query(
+    "SELECT * FROM tasks ORDER BY createdAt ASC"
+  );
+  console.log(result);
+  res.json(result);
+};
 
-export const getTask = (req, res) => {
-    res.send('getting a task')
-}
+export const getTask = async (req, res) => {
+  const [result] = await pool.query("SELECT * FROM tasks WHERE id = ?", [
+    req.params.id,
+  ]);
+  if (result.length == 0)
+  return res.status(404).json({ message: "Task not found"});
+  res.json(result);
+};
 
 export const createTask = async (req, res) => {
-    const {title, description} = req.body
-    const [response] = await pool.query('INSERT INTO tasks(title, description) VALUES (?, ?)', [title, description])
-    res.json({
-        id: response.insertId,
-        title,
-        description,
-    });
-}
+  const { title, description } = req.body;
+  const [response] = await pool.query(
+    "INSERT INTO tasks(title, description) VALUES (?, ?)",
+    [title, description]
+  );
+  res.json({
+    id: response.insertId,
+    title,
+    description,
+  });
+};
 
 export const updateTask = (req, res) => {
-    res.send('updating the task')
-}
+  res.send("updating the task");
+};
 
 export const deleteTask = (req, res) => {
-    res.send('deleting the task')
-}
+  res.send("deleting the task");
+};
