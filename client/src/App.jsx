3a1a253ba5059a55961eafa88156aff26e9f79ef
Harold Montes <haroldjosehd24@gmail.com>
2023-04-06 16:41:01 -0400

    create task frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,19 @@ import {Routes, Route} from "react-router-dom"
 import TaskForm from "./pages/TaskForm"
 import TasksPage from "./pages/TasksPage"
 import NotFound from './pages/NotFound'
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
+    <>
+    <Navbar />
     <Routes>
       <Route path="/" element={ <TasksPage /> } />
       <Route path="/new" element={ <TaskForm /> } />
       <Route path="*" element={ <NotFound /> } />
     </Routes>
+    
+    </>
   )
 }
 
