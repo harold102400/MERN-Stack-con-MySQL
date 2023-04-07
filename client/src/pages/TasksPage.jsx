b347559b182d5ad5d30a5f4b9130af250a3c9688
Harold Montes <haroldjosehd24@gmail.com>
2023-04-07 18:23:48 -0400

    list tasks frontend component

--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { getTasksRequest } from "../api/tasks.api";
+import TaskCard from "../components/TaskCard";
 
 const TasksPage = () => {
-  return (
-    <div>TasksPage</div>
-  )
-}
 
-export default TasksPage
+  const [Tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    async function loadTasks() {
+      const response = await getTasksRequest();
+      setTasks(response.data);
+    }
+    loadTasks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Tasks</h1>
+      {
+        Tasks.map((task, index) => {
+          return <TaskCard task={task} key={index}/>
+        })
+      }
+    </div>
+  );
+};
+
+export default TasksPage;
